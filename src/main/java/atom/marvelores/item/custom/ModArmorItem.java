db37afcaf6f6b981cbd11515bf0ae81f12f3b16c
Mathis ALLEWEIRELDT <mathis.alleweireldt.etu@univ-lille.fr>
2026-08-22 22:29:08 +0200

    Add full armor effects.

--- src/main/java/atom/marvelores/item/custom/ModArmorItem.java
+++ src/main/java/atom/marvelores/item/custom/ModArmorItem.java
@@ -1,88 +1,92 @@
-/*package atom.marvelores.item.custom;
+package atom.marvelores.item.custom;
 
-import com.google.common.collect.ImmutableMap;
 import atom.marvelores.item.ModArmorMaterials;
-import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.EquippableComponent;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.equipment.ArmorMaterial;
-import net.minecraft.server.world.ServerWorld;
+import com.google.common.collect.ImmutableMap;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.equipment.ArmorMaterial;
+import net.minecraft.world.item.equipment.Equippable;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.Map;
 
 public class ModArmorItem extends Item {
-    private static final Map<ArmorMaterial, List<StatusEffectInstance>> MATERIAL_TO_EFFECT_MAP =
-            (new ImmutableMap.Builder<ArmorMaterial, List<StatusEffectInstance>>())
+    private static final Map<ArmorMaterial, List<MobEffectInstance>> MATERIAL_TO_EFFECT_MAP =
+            (new ImmutableMap.Builder<ArmorMaterial, List<MobEffectInstance>>())
                     .put(ModArmorMaterials.VIBRANIUM_ARMOR_MATERIAL,
-                            List.of(new StatusEffectInstance(StatusEffects.HASTE,
-                                            400, 0, false, false),
-                                    new StatusEffectInstance(StatusEffects.SPEED,
-                                            400, 0, false, false))).build();
+                            List.of(new MobEffectInstance(MobEffects.HASTE, 400, 0, false, false),
+                                    new MobEffectInstance(MobEffects.SPEED, 400, 0, false, false)))
+                    .build();
 
-    public ModArmorItem(Settings settings) {
-        super(settings);
+    public ModArmorItem(Properties properties) {
+        super(properties);
     }
 
     @Override
-    public void inventoryTick(ItemStack stack, ServerWorld world, Entity entity, @Nullable EquipmentSlot slot) {
-        if(!world.isClient()) {
-            if(entity instanceof PlayerEntity player) {
+    public void inventoryTick(ItemStack stack, ServerLevel level, Entity entity, @Nullable EquipmentSlot slot) {
+        if(!level.isClientSide()) {
+            if(entity instanceof Player player) {
                 if(hasFullSuitOfArmorOn(player)) {
                     evaluateArmorEffects(player);
                 }
             }
         }
 
-        super.inventoryTick(stack, world, entity, slot);
+        super.inventoryTick(stack, level, entity, slot);
     }
 
-    private void evaluateArmorEffects(PlayerEntity player) {
-        for (Map.Entry<ArmorMaterial, List<StatusEffectInstance>> entry : MATERIAL_TO_EFFECT_MAP.entrySet()) {
+    private void evaluateArmorEffects(Player player) {
+        for (Map.Entry<ArmorMaterial, List<MobEffectInstance>> entry : MATERIAL_TO_EFFECT_MAP.entrySet()) {
             ArmorMaterial mapArmorMaterial = entry.getKey();
-            List<StatusEffectInstance> mapStatusEffects = entry.getValue();
+            List<MobEffectInstance> mapStatusEffects = entry.getValue();
 
             if(hasCorrectArmorOn(mapArmorMaterial, player)) {
-                addStatusEffectForMaterial(player, mapArmorMaterial, mapStatusEffects);
+                addStatusEffectForMaterial(player, mapStatusEffects);
             }
         }
     }
 
-    private void addStatusEffectForMaterial(PlayerEntity player, ArmorMaterial mapArmorMaterial, List<StatusEffectInstance> mapStatusEffect) {
-        boolean hasPlayerEffect = mapStatusEffect.stream().allMatch(statusEffectInstance -> player.hasStatusEffect(statusEffectInstance.getEffectType()));
+    private void addStatusEffectForMaterial(Player player, List<MobEffectInstance> mapStatusEffect) {
+        boolean hasPlayerEffect = mapStatusEffect.stream().allMatch(MobEffectInstance
+                -> player.hasEffect(MobEffectInstance.getEffect()));
 
         if(!hasPlayerEffect) {
-            for (StatusEffectInstance instance : mapStatusEffect) {
-                player.addStatusEffect(new StatusEffectInstance(instance.getEffectType(),
-                        instance.getDuration(), instance.getAmplifier(), instance.isAmbient(), instance.shouldShowParticles()));
+            for (MobEffectInstance instance : mapStatusEffect) {
+                player.addEffect(new MobEffectInstance(instance));
             }
         }
     }
 
-    private boolean hasCorrectArmorOn(ArmorMaterial material, PlayerEntity player) {
-        EquippableComponent equippableComponentBoots =     player.getEquippedStack(EquipmentSlot.FEET).getItem().getComponents().get(DataComponentTypes.EQUIPPABLE);
-        EquippableComponent equippableComponentLeggings =  player.getEquippedStack(EquipmentSlot.LEGS).getItem().getComponents().get(DataComponentTypes.EQUIPPABLE);
-        EquippableComponent equippableComponentBreastplate =  player.getEquippedStack(EquipmentSlot.CHEST).getItem().getComponents().get(DataComponentTypes.EQUIPPABLE);
-        EquippableComponent equippableComponentHelmet = player.getEquippedStack(EquipmentSlot.HEAD).getItem().getComponents().get(DataComponentTypes.EQUIPPABLE);
+    private boolean hasFullSuitOfArmorOn(Player player) {
+        ItemStack boots = player.getItemBySlot(EquipmentSlot.FEET);
+        ItemStack leggings = player.getItemBySlot(EquipmentSlot.LEGS);
+        ItemStack chestplate = player.getItemBySlot(EquipmentSlot.CHEST);
+        ItemStack helmet = player.getItemBySlot(EquipmentSlot.HEAD);
+
+        return !helmet.isEmpty() && !chestplate.isEmpty()
+                && !leggings.isEmpty() && !boots.isEmpty();
+    }
+
+    private boolean hasCorrectArmorOn(ArmorMaterial material, Player player) {
+        ItemStack boots = player.getItemBySlot(EquipmentSlot.FEET);
+        ItemStack leggings = player.getItemBySlot(EquipmentSlot.LEGS);
+        ItemStack chestplate = player.getItemBySlot(EquipmentSlot.CHEST);
+        ItemStack helmet = player.getItemBySlot(EquipmentSlot.HEAD);
+
+        Equippable equippableComponentBoots = boots.getComponents().get(DataComponents.EQUIPPABLE);
+        Equippable equippableComponentLeggings = leggings.getComponents().get(DataComponents.EQUIPPABLE);
+        Equippable equippableComponentBreastplate = chestplate.getComponents().get(DataComponents.EQUIPPABLE);
+        Equippable equippableComponentHelmet = helmet.getComponents().get(DataComponents.EQUIPPABLE);
 
         return equippableComponentBoots.assetId().get().equals(material.assetId()) && equippableComponentLeggings.assetId().get().equals(material.assetId()) &&
                 equippableComponentBreastplate.assetId().get().equals(material.assetId()) && equippableComponentHelmet.assetId().get().equals(material.assetId());
     }
-
-    private boolean hasFullSuitOfArmorOn(PlayerEntity player) {
-        ItemStack boots = player.getEquippedStack(EquipmentSlot.FEET);
-        ItemStack leggings = player.getEquippedStack(EquipmentSlot.LEGS);
-        ItemStack breastplate = player.getEquippedStack(EquipmentSlot.CHEST);
-        ItemStack helmet = player.getEquippedStack(EquipmentSlot.HEAD);
-
-        return !helmet.isEmpty() && !breastplate.isEmpty()
-                && !leggings.isEmpty() && !boots.isEmpty();
-    }
-}*/
+}
